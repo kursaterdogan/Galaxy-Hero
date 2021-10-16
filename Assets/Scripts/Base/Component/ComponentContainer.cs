1f1e861b5ac0056e237a57120086cefd04ffb40b
kursaterdogan <soullreaver@icloud.com>
2021-10-16 23:31:52 +0300

    feat(core): Create ComponentContainer

---
+++ Assets/Scripts/Base/Component/ComponentContainer.cs
@@ -0,0 +1,29 @@
+namespace Base.Component
+{
+    using System.Collections.Generic;
+
+    public class ComponentContainer
+    {
+        private Dictionary<string, object> components;
+
+        public ComponentContainer()
+        {
+            components = new Dictionary<string, object>();
+        }
+
+        public void AddComponent(string componentKey, object component)
+        {
+            components.Add(componentKey, component);
+        }
+
+        public object GetComponent(string componentKey)
+        {
+            if (!components.ContainsKey(componentKey))
+            {
+                return null;
+            }
+
+            return components[componentKey];
+        }
+    }
+}
