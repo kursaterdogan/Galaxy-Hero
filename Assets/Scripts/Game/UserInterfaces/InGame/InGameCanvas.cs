a8c84fa50e634f88cc954073ee1e5aa6a9416ae2
kursaterdogan <soullreaver@icloud.com>
2021-11-04 23:40:31 +0300

    feat(inGame): Create InGameCanvas

---
+++ Assets/Scripts/Game/UserInterfaces/InGame/InGameCanvas.cs
@@ -0,0 +1,20 @@
+namespace Game.UserInterfaces.InGame
+{
+    using Base.UserInterface;
+
+    public class InGameCanvas : BaseCanvas
+    {
+        public delegate void ReturnToMainMenuDelegate();
+
+        public event ReturnToMainMenuDelegate OnReturnToMainMenu;
+
+        public void RequestMainMenu()
+        {
+            //TODO Link with pause
+            if (OnReturnToMainMenu != null)
+            {
+                OnReturnToMainMenu();
+            }
+        }
+    }
+}
