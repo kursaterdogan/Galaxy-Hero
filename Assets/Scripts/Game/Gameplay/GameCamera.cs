e8f2172708e96887874a9348f183bee9a48b5029
kursaterdogan <soullreaver@icloud.com>
2021-12-02 15:11:28 +0300

    chore(gameCamera): Add TODO If IsLevelComplete

--- Assets/Scripts/Game/Gameplay/GameCamera.cs
+++ Assets/Scripts/Game/Gameplay/GameCamera.cs
@@ -6,6 +6,7 @@ namespace Game.Gameplay
     public class GameCamera : MonoBehaviour
     {
         //TODO Integrate with StateMachine
+        //TODO If IsLevelComplete => Change PassiveMoveSpeed Before OnExit
         private Camera _mainCamera;
         private int _screenBoundsWidth;
         private int _screenBoundsHeight;
