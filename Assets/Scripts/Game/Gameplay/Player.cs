34961b28fede51b94f503a8f0fd8ec27428a1a4f
kursaterdogan <soullreaver@icloud.com>
2021-12-20 22:18:00 +0300

    feat(projectile): Create ProjectileShredder

--- Assets/Scripts/Game/Gameplay/Player.cs
+++ Assets/Scripts/Game/Gameplay/Player.cs
@@ -2,6 +2,7 @@ namespace Game.Gameplay
 {
     using UnityEngine;
     using UnityEngine.InputSystem;
+    using System.Collections;
 
     public class Player : MonoBehaviour
     {
@@ -10,12 +11,21 @@ namespace Game.Gameplay
         private GameCamera _gameCamera;
         private float _moveSpeed = 5f;
 
+        private ProjectilePool _projectilePool;
+        private Coroutine _firingCoroutine;
+        [SerializeField] private Transform firingPoint;
+        float _projectileSpeed = 15f;
+        float _projectileFiringPeriod = 0.2f;
+
         private void Start()
         {
             //TODO SetMoveSpeed
             //TODO SetTimeScale on InGameState
+            //TODO Set ProjectileSpeed & ProjectileFiringPeriod
             Time.timeScale = 0.5f;
             SetGameCamera();
+            SetProjectilePool();
+            StartFiringCoroutine();
         }
 
         private void Update()
@@ -26,6 +36,21 @@ namespace Game.Gameplay
         private void SetGameCamera()
         {
             _gameCamera = FindObjectOfType<GameCamera>();
+        }
+
+        private void SetProjectilePool()
+        {
+            _projectilePool = FindObjectOfType<ProjectilePool>();
+        }
+
+        private void StartFiringCoroutine()
+        {
+            _firingCoroutine = StartCoroutine(FireContinously());
+        }
+
+        private void StopFiringCoroutine()
+        {
+            StopCoroutine(_firingCoroutine);
         }
 
         public void OnMove(InputAction.CallbackContext callbackContext)
@@ -53,8 +78,20 @@ namespace Game.Gameplay
 
         private void OnTriggerEnter2D(Collider2D col)
         {
-            //TODO Check EnemyTrigger
-            Debug.Log(col.name);
+            // TODO Check EnemyTrigger
+            // Debug.Log(col.name);
+        }
+
+        IEnumerator FireContinously()
+        {
+            while (true)
+            {
+                GameObject laser = _projectilePool.GetPlayerProjectile();
+                laser.transform.position = firingPoint.position;
+                laser.SetActive(true);
+                laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, _projectileSpeed);
+                yield return new WaitForSeconds(_projectileFiringPeriod);
+            }
         }
     }
 }
---
+++ Assets/Scripts/Game/Gameplay/ProjectileShredder.cs
@@ -0,0 +1,18 @@
+namespace Game.Gameplay
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class ProjectileShredder : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            Debug.Log(col.name);
+            if (col.gameObject.CompareTag("PlayerProjectile"))
+            {
+                col.gameObject.SetActive(false);
+            }
+        }
+    }
+}
