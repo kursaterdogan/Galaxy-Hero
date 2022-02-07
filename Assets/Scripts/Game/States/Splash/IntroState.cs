5b953151ca9284d5940cecb52b0c52f7811bab01
kursaterdogan <soullreaver@icloud.com>
2022-02-08 00:08:26 +0300

    feat(introState): Add OnIntroAnimationRequest

--- Assets/Scripts/Game/States/Splash/IntroState.cs
+++ Assets/Scripts/Game/States/Splash/IntroState.cs
@@ -2,38 +2,49 @@ using UnityEngine;
 using Base.Component;
 using Base.State;
 using Game.Components;
+using Game.UserInterfaces.Splash;
 
 namespace Game.States.Splash
 {
     public class IntroState : StateMachine
     {
+        private UIComponent _uiComponent;
         private IntroComponent _introComponent;
+        
+        private SplashCanvas _splashCanvas;
 
         public IntroState(ComponentContainer componentContainer)
         {
             _introComponent = componentContainer.GetComponent("IntroComponent") as IntroComponent;
+            _uiComponent = componentContainer.GetComponent("UIComponent") as UIComponent;
+
+            _splashCanvas = _uiComponent.GetCanvas(UIComponent.MenuName.Splash) as SplashCanvas;
         }
 
         protected override void OnEnter()
         {
             Debug.Log("IntroState OnEnter");
 
-            _introComponent.StartIntro();
+            _splashCanvas.OnIntroAnimationRequest += OnIntroAnimationRequest;
+
+            _introComponent.CallStart();
         }
 
         protected override void OnUpdate()
         {
             Debug.Log("IntroState OnUpdate");
-
-            if (_introComponent.IsIntroAnimationCompleted())
-            {
-                SendTrigger((int)StateTriggers.SplashLoading);
-            }
         }
 
         protected override void OnExit()
         {
             Debug.Log("IntroState OnExit");
+
+            _splashCanvas.OnIntroAnimationRequest -= OnIntroAnimationRequest;
+        }
+
+        private void OnIntroAnimationRequest()
+        {
+            SendTrigger((int)StateTriggers.SplashLoading);
         }
     }
 }
