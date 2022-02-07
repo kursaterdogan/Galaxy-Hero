2c07c973d05ecd30865f396e25c8538d9c3c3553
kursaterdogan <soullreaver@icloud.com>
2022-02-08 00:06:28 +0300

    feat(splashCanvas): Add SplashRequestDelegate

--- Assets/Scripts/Game/UserInterfaces/Splash/SplashCanvas.cs
+++ Assets/Scripts/Game/UserInterfaces/Splash/SplashCanvas.cs
@@ -6,36 +6,49 @@ namespace Game.UserInterfaces.Splash
 {
     public class SplashCanvas : BaseCanvas
     {
-        [SerializeField] private Logo appLogo;
+        public delegate void SplashRequestDelegate();
+
+        public event SplashRequestDelegate OnIntroAnimationRequest;
+        public event SplashRequestDelegate OnLoadingAnimationRequest;
+
+        [SerializeField] private Logo logo;
         [SerializeField] private LoadingIcon loadingIcon;
 
         private const float AnimationTime = 1.0f;
-        private bool _isIntroAnimCompleted;
 
         public void PlayIntroAnimation()
         {
             StartCoroutine(IntroAnimation());
         }
 
-        public bool IsIntroCompleted()
-        {
-            return _isIntroAnimCompleted;
-        }
-
         private IEnumerator IntroAnimation()
         {
-            appLogo.gameObject.SetActive(true);
-
-            appLogo.PlayFadeInAnimation(AnimationTime);
+            logo.gameObject.SetActive(true);
+            logo.PlayFadeInAnimation(AnimationTime);
             yield return new WaitForSeconds(AnimationTime);
+            logo.PlayFadeOutAnimation(AnimationTime);
 
-            appLogo.PlayFadeOutAnimation(AnimationTime);
-            yield return new WaitForSeconds(AnimationTime);
-
-            _isIntroAnimCompleted = true;
+            RequestIntroAnimation();
 
             loadingIcon.gameObject.SetActive(true);
             loadingIcon.PlayLoadingAnimation();
+            yield return new WaitForSeconds(AnimationTime);
+            loadingIcon.StopLoadingAnimation();
+
+            RequestLoadingAnimation();
+
+            logo.gameObject.SetActive(false);
+            loadingIcon.gameObject.SetActive(false);
+        }
+
+        private void RequestIntroAnimation()
+        {
+            OnIntroAnimationRequest?.Invoke();
+        }
+
+        private void RequestLoadingAnimation()
+        {
+            OnLoadingAnimationRequest?.Invoke();
         }
     }
 }
