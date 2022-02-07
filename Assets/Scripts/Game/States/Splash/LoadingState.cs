21a93a89046b92e8014f806477ee3c6a21d9fa12
kursaterdogan <soullreaver@icloud.com>
2022-02-08 00:07:04 +0300

    feat(loadingState): Add OnLoadingAnimationRequest

--- Assets/Scripts/Game/States/Splash/LoadingState.cs
+++ Assets/Scripts/Game/States/Splash/LoadingState.cs
@@ -1,32 +1,46 @@
 using UnityEngine;
+using Base.Component;
 using Base.State;
+using Game.Components;
+using Game.UserInterfaces.Splash;
 
 namespace Game.States.Splash
 {
     public class LoadingState : StateMachine
     {
-        private const float FakeLoadingTime = 1f;
-        private float _time;
+        private UIComponent _uiComponent;
+
+        private SplashCanvas _splashCanvas;
+
+        public LoadingState(ComponentContainer componentContainer)
+        {
+            _uiComponent = componentContainer.GetComponent("UIComponent") as UIComponent;
+
+            _splashCanvas = _uiComponent.GetCanvas(UIComponent.MenuName.Splash) as SplashCanvas;
+        }
 
         protected override void OnEnter()
         {
             Debug.Log("LoadingState OnEnter");
+
+            _splashCanvas.OnLoadingAnimationRequest += OnLoadingAnimationRequest;
         }
 
         protected override void OnUpdate()
         {
             Debug.Log("LoadingState OnUpdate");
-
-            _time += Time.deltaTime;
-            if (_time > FakeLoadingTime)
-            {
-                SendTrigger((int)StateTriggers.SplashCompleted);
-            }
         }
 
         protected override void OnExit()
         {
             Debug.Log("LoadingState OnExit");
+
+            _splashCanvas.OnLoadingAnimationRequest += OnLoadingAnimationRequest;
+        }
+
+        private void OnLoadingAnimationRequest()
+        {
+            SendTrigger((int)StateTriggers.SplashCompleted);
         }
     }
 }
