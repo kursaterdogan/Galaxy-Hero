9459501a39a2f4aebac4c0d2a56d006afce7b3cc
kursaterdogan <soullreaver@icloud.com>
2022-02-08 00:04:04 +0300

    refactor(logo): Remove Image null check

--- Assets/Scripts/Game/UserInterfaces/Splash/Logo.cs
+++ Assets/Scripts/Game/UserInterfaces/Splash/Logo.cs
@@ -10,21 +10,11 @@ namespace Game.UserInterfaces.Splash
 
         public void PlayFadeInAnimation(float duration)
         {
-            if (image == null)
-            {
-                return;
-            }
-
             image.DOColor(new Color(image.color.r, image.color.g, image.color.b, 1), duration).SetEase(Ease.InOutSine);
         }
 
         public void PlayFadeOutAnimation(float duration)
         {
-            if (image == null)
-            {
-                return;
-            }
-
             image.DOColor(new Color(image.color.r, image.color.g, image.color.b, 0), duration).SetEase(Ease.InOutSine);
         }
     }
