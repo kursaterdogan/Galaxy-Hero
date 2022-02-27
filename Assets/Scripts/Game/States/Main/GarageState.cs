8e48acab161f95e2a3fb3331d9ff2fc90b4f1787
kursaterdogan <soullreaver@icloud.com>
2022-02-28 02:05:44 +0300

    feat(main): Create GarageState

---
+++ Assets/Scripts/Game/States/Main/GarageState.cs
@@ -0,0 +1,24 @@
+using Base.Component;
+using Base.State;
+
+namespace Game.States.Main
+{
+    public class GarageState : StateMachine
+    {
+        public GarageState(ComponentContainer componentContainer)
+        {
+            //TODO Initialize GarageCanvas
+            //TODO Add Components
+        }
+
+        protected override void OnEnter()
+        {
+            //TODO Handle OnEnter
+        }
+
+        protected override void OnExit()
+        {
+            //TODO Handle OnExit
+        }
+    }
+}
