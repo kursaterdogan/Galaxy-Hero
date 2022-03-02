9afe7dcdefc311ee9429759d13f25af91f3b3676
kursaterdogan <soullreaver@icloud.com>
2022-03-02 23:29:48 +0300

    feat(main): Add GarageState

--- Assets/Scripts/Game/States/Main/MainState.cs
+++ Assets/Scripts/Game/States/Main/MainState.cs
@@ -8,17 +8,23 @@ namespace Game.States.Main
     {
         private MainMenuState _mainMenuState;
         private InventoryState _inventoryState;
+        private GarageState _garageState;
 
         public MainState(ComponentContainer componentContainer)
         {
             _mainMenuState = new MainMenuState(componentContainer);
             _inventoryState = new InventoryState(componentContainer);
+            _garageState = new GarageState(componentContainer);
 
             AddSubState(_mainMenuState);
             AddSubState(_inventoryState);
+            AddSubState(_garageState);
 
             AddTransition(_mainMenuState, _inventoryState, (int)StateTriggers.GoToInventory);
             AddTransition(_inventoryState, _mainMenuState, (int)StateTriggers.ReturnToMainMenu);
+
+            AddTransition(_mainMenuState, _garageState, (int)StateTriggers.GoToGarage);
+            AddTransition(_garageState, _mainMenuState, (int)StateTriggers.ReturnToMainMenu);
         }
 
         protected override void OnEnter()
