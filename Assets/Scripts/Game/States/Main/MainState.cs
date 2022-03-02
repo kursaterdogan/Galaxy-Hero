870f489f04c8ec88cd9a30789bda69ed0da711d9
kursaterdogan <soullreaver@icloud.com>
2022-03-02 22:57:15 +0300

    feat(mainState): Add InventoryState

--- Assets/Scripts/Game/States/Main/MainState.cs
+++ Assets/Scripts/Game/States/Main/MainState.cs
@@ -7,19 +7,23 @@ namespace Game.States.Main
     public class MainState : StateMachine
     {
         private MainMenuState _mainMenuState;
+        private InventoryState _inventoryState;
 
         public MainState(ComponentContainer componentContainer)
         {
             _mainMenuState = new MainMenuState(componentContainer);
+            _inventoryState = new InventoryState(componentContainer);
 
             AddSubState(_mainMenuState);
+            AddSubState(_inventoryState);
+
+            AddTransition(_mainMenuState, _inventoryState, (int)StateTriggers.GoToInventory);
+            AddTransition(_inventoryState, _mainMenuState, (int)StateTriggers.ReturnToMainMenu);
         }
 
         protected override void OnEnter()
         {
             Debug.Log("MainState OnEnter");
-            //TODO Check MainMenuDefaultState
-            // SetDefaultState();
         }
 
         protected override void OnExit()
