b2f9de71502c3c1f3a198af7f50e2891c86aa7fa
kursaterdogan <soullreaver@icloud.com>
2022-03-22 23:41:16 +0300

    chore(creditsComponent): Remove TODO

--- Assets/Scripts/Game/Components/CreditsComponent.cs
+++ Assets/Scripts/Game/Components/CreditsComponent.cs
@@ -8,7 +8,6 @@ namespace Game.Components
         public void Initialize(ComponentContainer componentContainer)
         {
             Debug.Log("<color=lime>" + gameObject.name + " initialized!</color>");
-            //TODO Handle CreditsComponent
         }
     }
 }
