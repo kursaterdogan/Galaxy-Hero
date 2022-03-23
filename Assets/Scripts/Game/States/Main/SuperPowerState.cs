8436b16aa95d00844e2159d0fefdebe84cce3884
kursaterdogan <soullreaver@icloud.com>
2022-03-24 00:48:41 +0300

    feat(superPowerState): Add Requests & Changes

--- Assets/Scripts/Game/States/Main/SuperPowerState.cs
+++ Assets/Scripts/Game/States/Main/SuperPowerState.cs
@@ -5,12 +5,12 @@ using Game.UserInterfaces.Main;
 
 namespace Game.States.Main
 {
-    public class SuperPowerState : StateMachine, IRequestable
+    public class SuperPowerState : StateMachine, IChangeable, IRequestable
     {
-        private UIComponent _uiComponent;
-        private SuperPowerComponent _superPowerComponent;
+        private readonly UIComponent _uiComponent;
+        private readonly SuperPowerComponent _superPowerComponent;
 
-        private SuperPowerCanvas _superPowerCanvas;
+        private readonly SuperPowerCanvas _superPowerCanvas;
 
         public SuperPowerState(ComponentContainer componentContainer)
         {
@@ -22,29 +22,122 @@ namespace Game.States.Main
 
         protected override void OnEnter()
         {
-            _uiComponent.EnableCanvas(UIComponent.MenuName.SuperPower);
-
+            SubscribeToComponentChangeDelegates();
             SubscribeToCanvasRequestDelegates();
+
+            _superPowerComponent.OnConstruct();
+
+            _uiComponent.EnableCanvas(UIComponent.MenuName.SuperPower);
         }
 
         protected override void OnExit()
         {
+            _superPowerComponent.OnDestruct();
+            _superPowerCanvas.OnQuit();
+
+            UnsubscribeToComponentChangeDelegates();
             UnsubscribeToCanvasRequestDelegates();
+        }
+
+        public void SubscribeToComponentChangeDelegates()
+        {
+            _superPowerComponent.OnShildeoDescriptionChange += ChangeShildeoDescriptionText;
+            _superPowerComponent.OnBombeoDescriptionChange += ChangeBombeoDescriptionText;
+            _superPowerComponent.OnGhosteoDescriptionChange += ChangeGhosteoDescriptionText;
+            _superPowerComponent.OnLeftSelectionButtonInteractableChange += ChangeLeftSelectionButtonInteractable;
+            _superPowerComponent.OnRightSelectionButtonInteractableChange += ChangeRightSelectionButtonInteractable;
+            _superPowerComponent.OnActivateShildeo += ActivateShildeo;
+            _superPowerComponent.OnActivateBombeo += ActivateBombeo;
+            _superPowerComponent.OnActivateGhosteo += ActivateGhosteo;
+        }
+
+        public void UnsubscribeToComponentChangeDelegates()
+        {
+            _superPowerComponent.OnShildeoDescriptionChange -= ChangeShildeoDescriptionText;
+            _superPowerComponent.OnBombeoDescriptionChange -= ChangeBombeoDescriptionText;
+            _superPowerComponent.OnGhosteoDescriptionChange -= ChangeGhosteoDescriptionText;
+            _superPowerComponent.OnLeftSelectionButtonInteractableChange -= ChangeLeftSelectionButtonInteractable;
+            _superPowerComponent.OnRightSelectionButtonInteractableChange -= ChangeRightSelectionButtonInteractable;
+            _superPowerComponent.OnActivateShildeo -= ActivateShildeo;
+            _superPowerComponent.OnActivateBombeo -= ActivateBombeo;
+            _superPowerComponent.OnActivateGhosteo -= ActivateGhosteo;
         }
 
         public void SubscribeToCanvasRequestDelegates()
         {
+            _superPowerCanvas.OnLeftSelectionRequest += RequestLeftSelection;
+            _superPowerCanvas.OnRightSelectionRequest += RequestRightSelection;
             _superPowerCanvas.OnReturnToMainMenuRequest += RequestReturnToMainMenu;
         }
 
         public void UnsubscribeToCanvasRequestDelegates()
         {
+            _superPowerCanvas.OnLeftSelectionRequest -= RequestLeftSelection;
+            _superPowerCanvas.OnRightSelectionRequest -= RequestRightSelection;
             _superPowerCanvas.OnReturnToMainMenuRequest -= RequestReturnToMainMenu;
+        }
+
+        #region Changes
+
+        private void ChangeShildeoDescriptionText(string description)
+        {
+            _superPowerCanvas.SetShildeoDescriptionText(description);
+        }
+
+        private void ChangeBombeoDescriptionText(string description)
+        {
+            _superPowerCanvas.SetBombeoDescriptionText(description);
+        }
+
+        private void ChangeGhosteoDescriptionText(string description)
+        {
+            _superPowerCanvas.SetGhosteoDescriptionText(description);
+        }
+
+        private void ChangeLeftSelectionButtonInteractable(bool isInteractable)
+        {
+            _superPowerCanvas.SetLeftSelectionButtonInteractable(isInteractable);
+        }
+
+        private void ChangeRightSelectionButtonInteractable(bool isInteractable)
+        {
+            _superPowerCanvas.SetRightSelectionButtonInteractable(isInteractable);
+        }
+
+        private void ActivateShildeo()
+        {
+            _superPowerCanvas.ActivateShildeo();
+        }
+
+        private void ActivateBombeo()
+        {
+            _superPowerCanvas.ActivateBombeo();
+        }
+
+        private void ActivateGhosteo()
+        {
+            _superPowerCanvas.ActivateGhosteo();
+        }
+
+        #endregion
+
+        #region Request
+
+        private void RequestLeftSelection()
+        {
+            _superPowerComponent.RequestLeftSelectionButton();
+        }
+
+        private void RequestRightSelection()
+        {
+            _superPowerComponent.RequestRightSelectionButton();
         }
 
         private void RequestReturnToMainMenu()
         {
             SendTrigger((int)StateTriggers.ReturnToMainMenu);
         }
+
+        #endregion
     }
 }
