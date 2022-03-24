8097dfbe5efcdeca3a70bd855241793362033876
kursaterdogan <soullreaver@icloud.com>
2022-03-24 16:09:00 +0300

    feat(endGameState): Add IChangeable & IRequestable

--- Assets/Scripts/Game/States/InGame/EndGameState.cs
+++ Assets/Scripts/Game/States/InGame/EndGameState.cs
@@ -1,21 +1,61 @@
 using Base.State;
 using Base.Component;
+using Game.Components;
+using Game.UserInterfaces.InGame;
 
 namespace Game.States.InGame
 {
-    public class EndGameState : StateMachine
+    public class EndGameState : StateMachine, IChangeable, IRequestable
     {
+        private readonly UIComponent _uiComponent;
+        private readonly EndGameComponent _endGameComponent;
+
+        private readonly EndGameCanvas _endGameCanvas;
+
         public EndGameState(ComponentContainer componentContainer)
         {
-            //TODO Hande EndGameState
+            //TODO Handle EndGameState
+            _uiComponent = componentContainer.GetComponent("UIComponent") as UIComponent;
+            _endGameComponent = componentContainer.GetComponent("EndGameComponent") as EndGameComponent;
+
+            _endGameCanvas = _uiComponent.GetCanvas(UIComponent.MenuName.EndGame) as EndGameCanvas;
         }
 
         protected override void OnEnter()
         {
+            SubscribeToComponentChangeDelegates();
+            SubscribeToCanvasRequestDelegates();
+
+            _uiComponent.EnableCanvas(UIComponent.MenuName.EndGame);
         }
 
         protected override void OnExit()
         {
+            UnsubscribeToComponentChangeDelegates();
+            UnsubscribeToCanvasRequestDelegates();
+        }
+
+        public void SubscribeToComponentChangeDelegates()
+        {
+        }
+
+        public void UnsubscribeToComponentChangeDelegates()
+        {
+        }
+
+        public void SubscribeToCanvasRequestDelegates()
+        {
+            _endGameCanvas.OnReturnToMainMenuRequest += RequestReturnToMainMenu;
+        }
+
+        public void UnsubscribeToCanvasRequestDelegates()
+        {
+            _endGameCanvas.OnReturnToMainMenuRequest -= RequestReturnToMainMenu;
+        }
+
+        private void RequestReturnToMainMenu()
+        {
+            SendTrigger((int)StateTriggers.ReturnToMainMenu);
         }
     }
 }
