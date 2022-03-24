dbdab7b29c41c1684bc3cd134dc96ce089cda4b6
kursaterdogan <soullreaver@icloud.com>
2022-03-24 16:01:50 +0300

    feat(prepareGameState): Add IChangeable

--- Assets/Scripts/Game/States/InGame/PrepareGameState.cs
+++ Assets/Scripts/Game/States/InGame/PrepareGameState.cs
@@ -1,23 +1,52 @@
 using Base.State;
 using Base.Component;
+using Game.Components;
+using Game.UserInterfaces.InGame;
 
 namespace Game.States.InGame
 {
-    public class PrepareGameState : StateMachine
+    public class PrepareGameState : StateMachine, IChangeable
     {
+        private readonly UIComponent _uiComponent;
+        private readonly PrepareGameComponent _prepareGameComponent;
+
+        private readonly PrepareGameCanvas _prepareGameCanvas;
+
         public PrepareGameState(ComponentContainer componentContainer)
         {
-            //TODO Hande PrepareGameState
+            //TODO Handle PrepareGameState
+            _uiComponent = componentContainer.GetComponent("UIComponent") as UIComponent;
+            _prepareGameComponent = componentContainer.GetComponent("PrepareGameComponent") as PrepareGameComponent;
+
+            _prepareGameCanvas = _uiComponent.GetCanvas(UIComponent.MenuName.PrepareGame) as PrepareGameCanvas;
         }
 
         protected override void OnEnter()
         {
-            //TODO Add Provision canvas
-            RequestPlayGame();
+            SubscribeToComponentChangeDelegates();
+
+            _prepareGameComponent.OnConstruct();
+            _prepareGameCanvas.OnStart();
+
+            _uiComponent.EnableCanvas(UIComponent.MenuName.PrepareGame);
         }
 
         protected override void OnExit()
         {
+            _prepareGameComponent.OnConstruct();
+            _prepareGameCanvas.OnQuit();
+
+            UnsubscribeToComponentChangeDelegates();
+        }
+
+        public void SubscribeToComponentChangeDelegates()
+        {
+            _prepareGameComponent.OnLoadingComplete += RequestPlayGame;
+        }
+
+        public void UnsubscribeToComponentChangeDelegates()
+        {
+            _prepareGameComponent.OnLoadingComplete -= RequestPlayGame;
         }
 
         private void RequestPlayGame()
