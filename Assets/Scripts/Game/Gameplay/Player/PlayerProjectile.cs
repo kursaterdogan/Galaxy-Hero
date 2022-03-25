eea527f8fff3898203bfa01441e359d04e0cb595
kursaterdogan <soullreaver@icloud.com>
2022-03-25 23:13:14 +0300

    feat(playerProjectile): Add SetVelocity

--- Assets/Scripts/Game/Gameplay/Player/PlayerProjectile.cs
+++ Assets/Scripts/Game/Gameplay/Player/PlayerProjectile.cs
@@ -4,11 +4,13 @@ namespace Game.Gameplay.Player
 {
     public class PlayerProjectile : MonoBehaviour
     {
+        [SerializeField] private Rigidbody2D rb2d;
+        private float _projectileSpeed = 10f;
         private float _maxVerticalPosition;
 
-        void Start()
+        void OnEnable()
         {
-            SetMaxVerticalPosition();
+            SetVelocity();
         }
 
         void Update()
@@ -16,10 +18,24 @@ namespace Game.Gameplay.Player
             CheckDestroyPosition();
         }
 
-        private void SetMaxVerticalPosition()
+        public void SetMaxVerticalPosition(float maxVerticalPosition)
         {
-            GameCamera gameCamera = FindObjectOfType<GameCamera>();
-            _maxVerticalPosition = gameCamera.GetMaxVerticalPosition();
+            _maxVerticalPosition = maxVerticalPosition;
+        }
+
+        public void SetPosition(Vector3 position)
+        {
+            transform.position = position;
+        }
+
+        public void SetRotation(Quaternion rotation)
+        {
+            transform.rotation = rotation;
+        }
+
+        private void SetVelocity()
+        {
+            rb2d.velocity = new Vector2(0, _projectileSpeed);
         }
 
         private void CheckDestroyPosition()
