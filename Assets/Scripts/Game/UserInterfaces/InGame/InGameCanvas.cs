2f414ca6cdab57d170d0261989e8cf55e6def948
kursaterdogan <soullreaver@icloud.com>
2022-03-29 18:51:02 +0300

    feat(inGameCanvas): Add Changes

--- Assets/Scripts/Game/UserInterfaces/InGame/InGameCanvas.cs
+++ Assets/Scripts/Game/UserInterfaces/InGame/InGameCanvas.cs
@@ -1,17 +1,79 @@
+using UnityEngine;
+using UnityEngine.UI;
 using Base.UserInterface;
+using TMPro;
 
 namespace Game.UserInterfaces.InGame
 {
-    public class InGameCanvas : BaseCanvas
+    public class InGameCanvas : BaseCanvas, IStartable
     {
         public delegate void InGameRequestDelegate();
-        
+
         public event InGameRequestDelegate OnReturnToMainMenuRequest;
+
+        private const string DefaultScoreValue = "0";
+        [SerializeField] private TMP_Text scoreText;
+
+        [SerializeField] private GameObject[] healthLevels;
+        [SerializeField] private Image[] currentHealthLevels;
+
+        public void OnStart()
+        {
+            ResetScoreText();
+            ResetHealthLevels();
+            ResetCurrentHealthLevels();
+        }
+
+        public void SetHealthLevels(int healthLevel)
+        {
+            for (int i = 0; i < healthLevel; i++)
+            {
+                if (!healthLevels[i].activeInHierarchy)
+                    healthLevels[i].SetActive(true);
+            }
+        }
+
+        public void SetCurrentHealthLevel(int currentHealthLevel)
+        {
+            ResetCurrentHealthLevels();
+
+            for (int i = 0; i < currentHealthLevel; i++)
+            {
+                currentHealthLevels[i].enabled = true;
+            }
+        }
+
+        public void ChangeScore(string score)
+        {
+            scoreText.text = score;
+        }
 
         public void RequestReturnToMainMenu()
         {
-            //TODO Handle InGameCanvas
             OnReturnToMainMenuRequest?.Invoke();
+        }
+
+        private void ResetHealthLevels()
+        {
+            foreach (GameObject healthLevel in healthLevels)
+            {
+                if (healthLevel.activeInHierarchy)
+                    healthLevel.SetActive(false);
+            }
+        }
+
+        private void ResetCurrentHealthLevels()
+        {
+            foreach (Image currentHealthLevel in currentHealthLevels)
+            {
+                if (currentHealthLevel.enabled)
+                    currentHealthLevel.enabled = false;
+            }
+        }
+
+        private void ResetScoreText()
+        {
+            scoreText.text = DefaultScoreValue;
         }
     }
 }
