f86bb0740c2fe28c390bf4e3dca6e2361d8abaab
kursaterdogan <soullreaver@icloud.com>
2022-03-29 21:58:02 +0300

    feat(endGameComponent): Add Changes

--- Assets/Scripts/Game/Components/EndGameComponent.cs
+++ Assets/Scripts/Game/Components/EndGameComponent.cs
@@ -7,14 +7,84 @@ namespace Game.Components
     {
         public delegate void EndGameChangeDelegate();
 
+        public event EndGameChangeDelegate OnWin;
+        public event EndGameChangeDelegate OnLose;
+
+        public delegate void EndGamePlanetStatusChangeDelegate(string text);
+
+        public event EndGamePlanetStatusChangeDelegate OnSavePlanetChange;
+        public event EndGamePlanetStatusChangeDelegate OnNeedYourHelpChange;
+
+        public delegate void EndGameTextChangeDelegate(string txt, string text);
+
+        public event EndGameTextChangeDelegate OnScoreChange;
+        public event EndGameTextChangeDelegate OnCoinChange;
+
+        private const string ScoreText = "Score";
+        private const string HighScoreText = "HighScore";
+        private const string SavePlanetText = "You Saved The ";
+        private const string NeedYourHelpText = " Need Your Help";
+
+        private DataComponent _dataComponent;
+        private MainMenuComponent _mainMenuComponent;
+        private InGameComponent _inGameComponent;
+
         public void Initialize(ComponentContainer componentContainer)
         {
-            //TODO Handle EndGameComponent
             Debug.Log("<color=lime>" + gameObject.name + " initialized!</color>");
+
+            _dataComponent = componentContainer.GetComponent("DataComponent") as DataComponent;
+            _mainMenuComponent = componentContainer.GetComponent("MainMenuComponent") as MainMenuComponent;
+            _inGameComponent = componentContainer.GetComponent("InGameComponent") as InGameComponent;
         }
 
         public void OnConstruct()
         {
+            ChangeHighScore();
+            ChangeCoin();
+            SetPlanetStatus();
         }
+
+        #region Changes
+
+        private void ChangeHighScore()
+        {
+            int highScore = _dataComponent.AchievementData.highScore;
+            int lastScore = _inGameComponent.LastScore;
+
+            if (lastScore > highScore)
+                OnScoreChange?.Invoke(HighScoreText, highScore.ToString());
+            else if (highScore >= lastScore)
+                OnScoreChange?.Invoke(ScoreText, lastScore.ToString());
+        }
+
+        private void ChangeCoin()
+        {
+            string ownedCoin = _dataComponent.CoinData.ownedCoin.ToString();
+            string lastGainedCoin = _inGameComponent.LastGainedCoin.ToString();
+
+            OnCoinChange?.Invoke(ownedCoin, lastGainedCoin);
+        }
+
+        private void SetPlanetStatus()
+        {
+            bool isPlanetSaved = _inGameComponent.IsPlanetSaved;
+            string selectedPlanet = _mainMenuComponent.GetSelectedPlanetName();
+
+            if (isPlanetSaved)
+            {
+                string savePlanetText = SavePlanetText + selectedPlanet;
+                OnSavePlanetChange?.Invoke(savePlanetText);
+                OnWin?.Invoke();
+            }
+            else
+            {
+                string needYourHelpText = selectedPlanet + NeedYourHelpText;
+                OnNeedYourHelpChange?.Invoke(needYourHelpText);
+                OnLose?.Invoke();
+            }
+        }
+
+        #endregion
     }
 }
