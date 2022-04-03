4dca3a2ce6f54ec2e282c0d4e8695f70bc3e4691
kursaterdogan <soullreaver@icloud.com>
2022-04-03 14:01:09 +0300

    feat(enums): Create StateTrigger

---
+++ Assets/Scripts/Game/Enums/StateTrigger.cs
@@ -0,0 +1,16 @@
+namespace Game.Enums
+{
+    public enum StateTrigger
+    {
+        GoToMainMenu,
+        ReturnToMainMenu,
+        GoToInventory,
+        GoToGarage,
+        GoToSuperPower,
+        GoToCredits,
+        GoToAchievement,
+        StartGame,
+        PlayGame,
+        EndGame,
+    }
+}
