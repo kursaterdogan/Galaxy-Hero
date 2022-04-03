1f33981cca0cc3c586194a5a06fb302539c04308
kursaterdogan <soullreaver@icloud.com>
2022-04-03 13:39:15 +0300

    feat(playerProjectile): Add OnTriggerEnter2D

--- Assets/Scripts/Game/Gameplay/Player/PlayerProjectile.cs
+++ Assets/Scripts/Game/Gameplay/Player/PlayerProjectile.cs
@@ -4,6 +4,12 @@ namespace Game.Gameplay.Player
 {
     public class PlayerProjectile : Projectile
     {
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Enemy"))
+                gameObject.SetActive(false);
+        }
+
         public void SetPosition(Vector3 position)
         {
             transform.position = position;
