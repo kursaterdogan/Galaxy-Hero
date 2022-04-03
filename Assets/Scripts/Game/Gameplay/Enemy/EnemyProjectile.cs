aaf473e3e964a2721cd00597967418af46382bde
kursaterdogan <soullreaver@icloud.com>
2022-04-03 13:39:27 +0300

    feat(enemyProjectile): Add OnTriggerEnter2D

--- Assets/Scripts/Game/Gameplay/Enemy/EnemyProjectile.cs
+++ Assets/Scripts/Game/Gameplay/Enemy/EnemyProjectile.cs
@@ -1,7 +1,15 @@
+using UnityEngine;
+
 namespace Game.Gameplay.Enemy
 {
     public class EnemyProjectile : Projectile
     {
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Player"))
+                Destroy(gameObject);
+        }
+
         protected override void CheckDestroyPosition()
         {
             if (transform.position.y < DestroyPosition)
