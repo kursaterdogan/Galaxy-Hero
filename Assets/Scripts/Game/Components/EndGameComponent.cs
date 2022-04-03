0932ce0962f2060c391a786c72e6fcce3db4277c
kursaterdogan <soullreaver@icloud.com>
2022-04-03 15:17:51 +0300

    style(components): EndGameComponent

--- Assets/Scripts/Game/Components/EndGameComponent.cs
+++ Assets/Scripts/Game/Components/EndGameComponent.cs
@@ -21,11 +21,11 @@ namespace Game.Components
         public event EndGameTextChangeDelegate OnScoreChange;
         public event EndGameTextChangeDelegate OnGoldChange;
 
-        private const string ScoreText = "Score";
-        private const string GoldPrefixText = "+";
-        private const string NewHighScoreText = "New High Score";
-        private const string SavePlanetText = "You Saved The ";
-        private const string NeedYourHelpText = " Need Your Help";
+        private const string _scoreText = "Score";
+        private const string _goldPrefixText = "+";
+        private const string _newHighScoreText = "New High Score";
+        private const string _savePlanetText = "You Saved The ";
+        private const string _needYourHelpText = " Need Your Help";
 
         private DataComponent _dataComponent;
         private InGameComponent _inGameComponent;
@@ -54,15 +54,15 @@ namespace Game.Components
             int lastScore = _inGameComponent.LastScore;
 
             if (isHighScore)
-                OnScoreChange?.Invoke(NewHighScoreText, highScore.ToString());
+                OnScoreChange?.Invoke(_newHighScoreText, highScore.ToString());
             else
-                OnScoreChange?.Invoke(ScoreText, lastScore.ToString());
+                OnScoreChange?.Invoke(_scoreText, lastScore.ToString());
         }
 
         private void ChangeGold()
         {
             string ownedCoin = _dataComponent.GoldData.ownedGold.ToString();
-            string lastGainedCoin = GoldPrefixText + _inGameComponent.LastGainedGold;
+            string lastGainedCoin = _goldPrefixText + _inGameComponent.LastGainedGold;
 
             OnGoldChange?.Invoke(ownedCoin, lastGainedCoin);
         }
@@ -74,13 +74,13 @@ namespace Game.Components
 
             if (isPlanetSaved)
             {
-                string savePlanetText = SavePlanetText + selectedPlanet;
+                string savePlanetText = _savePlanetText + selectedPlanet;
                 OnSavePlanetChange?.Invoke(savePlanetText);
                 OnWin?.Invoke();
             }
             else
             {
-                string needYourHelpText = selectedPlanet + NeedYourHelpText;
+                string needYourHelpText = selectedPlanet + _needYourHelpText;
                 OnNeedYourHelpChange?.Invoke(needYourHelpText);
                 OnLose?.Invoke();
             }
