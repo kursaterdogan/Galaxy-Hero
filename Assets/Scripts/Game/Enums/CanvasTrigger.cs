3a51344327fe3054ffd043a955b93a63ee785713
kursaterdogan <soullreaver@icloud.com>
2022-04-12 00:52:21 +0300

    feat(enums): Create CanvasTrigger

---
+++ Assets/Scripts/Game/Enums/CanvasTrigger.cs
@@ -0,0 +1,16 @@
+﻿namespace Game.Enums
+{
+    public enum CanvasTrigger
+    {
+        Intro,
+        MainMenu,
+        Inventory,
+        Garage,
+        SuperPower,
+        Credits,
+        Achievement,
+        PrepareGame,
+        InGame,
+        EndGame
+    }
+}
